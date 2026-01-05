7fe70fb97528ac85223a2f516c1f7e622061244a
arrifat <arifatdhivanahara916@gmail.com>
2026-01-05 14:09:54 +0700

    changes to footer section

--- src/components/organisms/footer/FooterSection.tsx
+++ src/components/organisms/footer/FooterSection.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { genreColumns, helpLinks } from "./FooterData"
-import Logo from "../../../assets/images/logo.png"
+import Logo from "../../../assets/images/Logo.png"
 
 const Footer: React.FC = () => {
   return (
